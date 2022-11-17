b33e841ac275869dff76141ebd8049f5974744e7
ngocing <mngoc010299@gmail.com>
2022-11-17 11:03:34 +0700

    save menu

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,13 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour
 {
+    int savedScene;
+    int sceneIndex;
     public void StartGame(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void QuitGame(){
         Application.Quit();
+    }
+    public void LoadGame(){
+        savedScene = PlayerPrefs.GetInt("Saved");
+        if(savedScene != 0){
+            SceneManager.LoadSceneAsync(savedScene);
+        }else
+            return;
+    }
+    public void SaveGame(){
+        sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        PlayerPrefs.SetInt("Saved", sceneIndex);
+        PlayerPrefs.Save();
+        // SceneManager.LoadSceneAsync(0);
     }
 }
